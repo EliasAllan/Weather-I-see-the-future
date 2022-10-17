416edf7a0050a0785da6f37b49238e39c321ea67
AllanEl <allanrnelias@gmail.com>
2022-10-17 19:49:12 -0400

    added css and header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+'getPlace'+'&appid=338ceedf83a992ffd42dcc24175384c9';
 console.log
 var cityName = document.getElementById("city");
 var stateCode = document.getElementById("state");
 var countryCode = document.getElementById("country");
 var getForecast = document.querySelector("#getForecast")
 
+// create
+
+
+// modify
+// append
+
 // 338ceedf83a992ffd42dcc24175384c9
 // '+'#city'+'#state'+'#country'+'
 
-city = '';
-state = '';
-country = '';
+// city = '';
+// state = '';
+// country = '';
 
-function getPlace(){
-  city = cityName.value;
-  state = stateCode.value;
-  country = countryCode.value;
-  console.log(cityName.value + ',' + stateCode.value + ',' + countryCode.value)
-  
+function getPlace(city, state, country){
+  // city = cityName.value;
+  // state = stateCode.value;
+  // country = countryCode.value;
+  // console.log(cityName.value + ',' + stateCode.value + ',' + countryCode.value)
+  // console.log(city, state, country)
+  return city + ',' + state + ',' + country;
 }
 
 function fetchAPI (){
+  var cityText = cityName.value;
+  var stateText = stateCode.value;
+  var countryText = countryCode.value;
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ getPlace(cityText, stateText, countryText) +'&appid=338ceedf83a992ffd42dcc24175384c9';
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city + ',' + state + ',' + country + '&appid=338ceedf83a992ffd42dcc24175384c9';
+// var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${state},${country}&appid=338ceedf83a992ffd42dcc24175384c9`;
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
