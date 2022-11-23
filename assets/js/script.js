2a4becba7dc6d12d1af198957e949c03d7a1256e
AllanEl <allanrnelias@gmail.com>
2022-11-23 02:58:23 -0500

    quickfix

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,7 @@ renderStorage();
 // country = '';
 
 function getValue(event) {
+  
   event.preventDefault()
   if(event.target.id === "getForecast"){
     var cityName = document.getElementById("city");
@@ -52,7 +53,9 @@ function getValue(event) {
     saveToStorage(cityName.value)
     return
   }
+  
   fetchAPI(event.target.innerHTML)
+  // futureForecasts.innerHTML = "";
 }
 
 function fetchAPI(cityParam) {
@@ -72,6 +75,10 @@ function fetchAPI(cityParam) {
     .then(function (data) {
       console.log("Github Repo Issues \n----------");
       console.log(data);
+      console.log(document.body)
+      console.log(typeof document.body)
+      
+      
       for (var i = 0; i < data.length; i++) {
         // console.log("url: " + data[i].url + " login: " + data[i].user.login)
         console.log(data[i]);
@@ -81,7 +88,9 @@ function fetchAPI(cityParam) {
       //create
       var currentForecastEl = document.querySelector("#currentForecast");
       currentForecastEl.innerHTML = "";
-
+      var futureForecasts = document.querySelector("#futureForecasts");
+      
+      
       // create
       var futureForecastsContainer = document.createElement("div")
 
@@ -90,6 +99,13 @@ function fetchAPI(cityParam) {
       var fiveDayForeDay3Card = document.createElement("div")
       var fiveDayForeDay4Card = document.createElement("div")
       var fiveDayForeDay5Card = document.createElement("div")
+
+      iconEl = document.createElement("img")
+      iconDay1 = document.createElement("img")
+      iconDay2 = document.createElement("img")
+      iconDay3 = document.createElement("img")
+      iconDay4 = document.createElement("img")
+      iconDay5 = document.createElement("img")
 
       var tempDay1 = document.createElement("h6")
       var tempDay2 = document.createElement("h6")
@@ -102,18 +118,34 @@ function fetchAPI(cityParam) {
       var windDay3 = document.createElement("h6")
       var windDay4 = document.createElement("h6")
       var windDay5 = document.createElement("h6")
-
+     
+      var humDay1 = document.createElement("h6")
+      var humDay2 = document.createElement("h6")
+      var humDay3 = document.createElement("h6")
+      var humDay4 = document.createElement("h6")
+      var humDay5 = document.createElement("h6")
+      
+      iconEl = document.createElement('img')
+      iconElDay1 = document.createElement('img')
+      iconElDay2 = document.createElement('img')
+      iconElDay3 = document.createElement('img')
+      iconElDay4 = document.createElement('img')
+      iconElDay5 = document.createElement('img')
+      
       var cityNameEl = document.createElement("p");
       var currentTempEl = document.createElement("p");
       var currentWindEl = document.createElement("p");
       var currentHumidityEl = document.createElement("p");
       // modify
+
+      futureForecastsContainer.setAttribute("id", "futureForecasts");
       fiveDayForeDay1Card.setAttribute("class", "cards");
       fiveDayForeDay2Card.setAttribute("class", "cards");
       fiveDayForeDay3Card.setAttribute("class", "cards");
       fiveDayForeDay4Card.setAttribute("class", "cards");
       fiveDayForeDay5Card.setAttribute("class", "cards");
 
+      
       // windDay1.setAttribute("class", "cards");
       // windDay2.setAttribute("class", "cards");
       // windDay3.setAttribute("class", "cards");
@@ -127,11 +159,17 @@ function fetchAPI(cityParam) {
       tempDay4.textContent = "Temp: " + Math.floor((data.list[31].main.temp - 273.15) * 1.8 + 32) + "°F";
       tempDay5.textContent = "Temp: " + Math.floor((data.list[39].main.temp - 273.15) * 1.8 + 32) + "°F";
 
-      windDay1.textContent = "Wind: " + data.list[7].wind.speed   + "MPH";
-      windDay2.textContent = "Wind: " + data.list[15].wind.speed  + "MPH";
-      windDay3.textContent = "Wind: " + data.list[23].wind.speed  + "MPH";
-      windDay4.textContent = "Wind: " + data.list[31].wind.speed  + "MPH";
-      windDay5.textContent = "Wind: " + data.list[39].wind.speed  + "MPH";
+      windDay1.textContent = "Wind: " + data.list[7].wind.speed   + " MPH";
+      windDay2.textContent = "Wind: " + data.list[15].wind.speed  + " MPH";
+      windDay3.textContent = "Wind: " + data.list[23].wind.speed  + " MPH";
+      windDay4.textContent = "Wind: " + data.list[31].wind.speed  + " MPH";
+      windDay5.textContent = "Wind: " + data.list[39].wind.speed  + " MPH";
+     
+      humDay1.textContent = "Humidity: " + data.list[7].main.humidity   + " %";
+      humDay2.textContent = "Humidity: " + data.list[15].main.humidity  + " %";
+      humDay3.textContent = "Humidity: " + data.list[23].main.humidity  + " %";
+      humDay4.textContent = "Humidity: " + data.list[31].main.humidity  + " %";
+      humDay5.textContent = "Humidity: " + data.list[39].main.humidity  + " %";
 
 
       // tempDay1.textContent = "Wind: " + data.list[0].wind.speed + " MPH";
@@ -147,20 +185,34 @@ function fetchAPI(cityParam) {
       currentTempEl.setAttribute("style", "font-size:25px");
 
       futureForecastsContainer.setAttribute("style", "display:flex");
-      // tempDay2.setAttribute("style", "margin-left:265px");
-      // tempDay3.setAttribute("style", "margin-left:265px");
-      // tempDay4.setAttribute("style", "margin-left:265px");
-      // tempDay5.setAttribute("style", "margin-left:265px");
-
       currentWindEl.setAttribute("style", "font-size:25px");
       currentHumidityEl.setAttribute("style", "font-size:25px");
-      iconEl = document.createElement('img')
+      
       iconLink = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+      iconLinkDay1 = "http://openweathermap.org/img/w/" + data.list[7].weather[0].icon + ".png";
+      iconLinkDay2 = "http://openweathermap.org/img/w/" + data.list[15].weather[0].icon + ".png";
+      iconLinkDay3 = "http://openweathermap.org/img/w/" + data.list[23].weather[0].icon + ".png";
+      iconLinkDay4 = "http://openweathermap.org/img/w/" + data.list[31].weather[0].icon + ".png";
+      iconLinkDay5 = "http://openweathermap.org/img/w/" + data.list[39].weather[0].icon + ".png";
+
       iconEl.setAttribute("src", iconLink)
-      console.log(iconEl)
-      document.body.children[1].children[1].append(iconEl)
+      iconElDay1.setAttribute("src", iconLinkDay1)
+      iconElDay2.setAttribute("src", iconLinkDay2)
+      iconElDay3.setAttribute("src", iconLinkDay3)
+      iconElDay4.setAttribute("src", iconLinkDay4)
+      iconElDay5.setAttribute("src", iconLinkDay5)
+
+      // console.log(iconEl)
+      
       // append
-      // searchContainer.appendChild(savedCityEl);
+      
+      document.body.children[1].children[1].append(iconEl)
+      fiveDayForeDay1Card.appendChild(iconElDay1);
+      fiveDayForeDay2Card.appendChild(iconElDay2);
+      fiveDayForeDay3Card.appendChild(iconElDay3);
+      fiveDayForeDay4Card.appendChild(iconElDay4);
+      fiveDayForeDay5Card.appendChild(iconElDay5);
+
 
       document.body.appendChild(futureForecastsContainer)
       futureForecastsContainer.appendChild(fiveDayForeDay1Card);
@@ -175,11 +227,23 @@ function fetchAPI(cityParam) {
       fiveDayForeDay4Card.appendChild(tempDay4);
       fiveDayForeDay5Card.appendChild(tempDay5);
 
+      fiveDayForeDay1Card.appendChild(tempDay1);
+      fiveDayForeDay2Card.appendChild(tempDay2);
+      fiveDayForeDay3Card.appendChild(tempDay3);
+      fiveDayForeDay4Card.appendChild(tempDay4);
+      fiveDayForeDay5Card.appendChild(tempDay5);
+
       fiveDayForeDay1Card.appendChild(windDay1);
       fiveDayForeDay2Card.appendChild(windDay2);
       fiveDayForeDay3Card.appendChild(windDay3);
       fiveDayForeDay4Card.appendChild(windDay4);
       fiveDayForeDay5Card.appendChild(windDay5);
+      
+      fiveDayForeDay1Card.appendChild(humDay1);
+      fiveDayForeDay2Card.appendChild(humDay2);
+      fiveDayForeDay3Card.appendChild(humDay3);
+      fiveDayForeDay4Card.appendChild(humDay4);
+      fiveDayForeDay5Card.appendChild(humDay5);
 
       document.body.children[1].children[1].appendChild(cityNameEl);
       document.body.children[1].children[1].appendChild(currentTempEl);
